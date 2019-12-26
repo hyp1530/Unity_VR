130da86fb021ff2fc8b4d171b38a47cc4655b4df
hyp1530 <55218857+hyp1530@users.noreply.github.com>
2019-12-26 10:22:22 +0800

    1226

---
+++ UnityVR/Assets/BigDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigDoor : MonoBehaviour {
+
+    private Animator ani;
+    private void Start()
+    {
+        ani = GetComponent<Animator>();
+    }
+    public void OpenDoor()
+    {
+        ani.SetBool("character_nearby", true);
+    }
+}
